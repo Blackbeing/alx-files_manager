81333bf7d0128a6a0853b627b825ffd0be0d8118
Blackbeing <jonnieey55@gmail.com>
2024-10-02 22:39:56 +0000

    feat file manager Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(err));
+    this.isConnected = false;
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+module.exports = new RedisClient();
